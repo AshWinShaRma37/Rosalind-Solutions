8233763bcc50579911f1f6b85cbf631f7ced4333
Ashwin <83164687+AshWinShaRma37@users.noreply.github.com>
2022-07-15 17:45:18 +0530

    Add files via upload

---
+++ Python Village/Conditions and Loops/sol.py
@@ -0,0 +1,26 @@
+#getting the input from file
+f = open('rosalind_ini4.txt')
+
+#reading the line and getting the string
+line1 = f.readline()
+
+#getting the calues
+values = line1.split()
+a = int(values[0])
+b = int(values[1])
+
+#creating a variable to store sum of odd integers:
+s = 0
+
+#creating a loop to find all odd integers and adding them to sum
+for i in range(a,b+1):
+         if i % 2 ==1:
+                  s=s+i
+
+#printing the result
+print(s)
+
+#another method
+#using range with step of 2 to get all the odd integers
+#used a|1 to make sure range starts from an odd integer
+print(sum(range(a|1,b+1,2)))

a28cbff599190aeeecf110d99573ff871db74bf9
Ashwin <83164687+AshWinShaRma37@users.noreply.github.com>
2022-07-15 17:11:41 +0530

    Add files via upload

---
+++ Python Village/Variables and Some Arithmetic/sol.py
@@ -0,0 +1,9 @@
+#getting the input from file
+f = open("rosalind_ini2.txt", "rt")
+#reading the line and getting the two integers
+line = f.read()
+values = line.split()
+a = int(values[0])
+b = int(values[1])
+#printing the hypotenuse of these numbers
+print(a**2 + b**2)
